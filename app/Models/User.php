8aa54095e7c13cb4a9453f432c221d97d4da4802
unknown <s.adeboye@outlook.com>
2024-09-24 08:41:23 +0100

    added user model

---
+++ app/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Log;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'gender',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /**
+     * Override the default sendEmailVerificationNotification method to add logging.
+     */
+    public function sendEmailVerificationNotification()
+    {
+        Log::info('Sending email verification notification to ' . $this->email);
+
+        // Send the default Laravel email verification notification
+        $this->notify(new \Illuminate\Auth\Notifications\VerifyEmail);
+    }
+}
