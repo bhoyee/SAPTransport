2e6ba2cbe28d8386d4ae938c473b791b409193d2
unknown <s.adeboye@outlook.com>
2024-09-24 10:15:06 +0100

    updated the passanger dashboard

--- resources/views/passenger/dashboard.blade.php
+++ resources/views/passenger/dashboard.blade.php
@@ -17,7 +17,7 @@
 							        <div>Ready to plan your next trip? Book your transport easily and get on the road!</div>
 							    </div><!--//col-->
 							    <div class="col-12 col-lg-3">
-								    <a class="btn app-btn-primary" href=""><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-arrow-down me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								    <a class="btn btn-primary" href="{{ url('/') }}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-arrow-down me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
   <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
   <path fill-rule="evenodd" d="M8 6a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 10.293V6.5A.5.5 0 0 1 8 6z"/>
@@ -295,7 +295,7 @@
 
 						    <div class="app-card-footer p-4 mt-auto">
 
-							   <a class="btn app-btn-secondary" href="#">View Invoices</a>
+							   <a class="btn btn-secondary" href="#">View Invoices</a>
 
 						    </div><!--//app-card-footer-->
 
@@ -349,7 +349,7 @@
 
 						    <div class="app-card-footer p-4 mt-auto">
 
-							   <a class="btn app-btn-secondary" href="#">Contact Support</a>
+							   <a class="btn btn-secondary" href="#">Contact Support</a>
 
 						    </div><!--//app-card-footer-->
 
@@ -403,7 +403,7 @@
 
 						    <div class="app-card-footer p-4 mt-auto">
 
-							   <a class="btn app-btn-secondary" href="#">Update Settings</a>
+							   <a class="btn btn-secondary" href="#">Update Settings</a>
 
 						    </div><!--//app-card-footer-->
 
