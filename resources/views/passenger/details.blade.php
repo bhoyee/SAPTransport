5c3c0eaed2662b087636d61250c4bce0dd2faa97
unknown <s.adeboye@outlook.com>
2024-09-26 13:23:43 +0100

    updated details view

--- resources/views/passenger/details.blade.php
+++ resources/views/passenger/details.blade.php
@@ -9,47 +9,112 @@
         <div class="app-card-body p-4">
             <h3 class="mb-3">Booking Information</h3>
             <div class="row gx-5 gy-3">
-                <div class="col-md-6">
-                    <strong>Service Type:</strong>
-                    <p>{{ $booking->service_type }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Booking Date:</strong>
-                    <p>{{ \Carbon\Carbon::parse($booking->created_at)->format('d M, Y') }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Vehicle Type:</strong>
-                    <p>{{ $booking->vehicle_type }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Status:</strong>
-                    <p>{{ $booking->status }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Pickup Address:</strong>
-                    <p>{{ $booking->pickup_address }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Dropoff Address:</strong>
-                    <p>{{ $booking->dropoff_address }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Pickup Date:</strong>
-                    <p>{{ \Carbon\Carbon::parse($booking->pickup_date)->format('d M, Y') }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Pickup Time:</strong>
-                    <p>{{ $booking->pickup_time }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Number of Adults:</strong>
-                    <p>{{ $booking->number_adults }}</p>
-                </div>
-                <div class="col-md-6">
-                    <strong>Number of Children:</strong>
-                    <p>{{ $booking->number_children }}</p>
-                </div>
-                <!-- Add more fields as needed -->
+                
+                <!-- Service Type -->
+                @if (!empty($booking->service_type))
+                    <div class="col-md-6">
+                        <strong>Service Type:</strong>
+                        <p>{{ $booking->service_type }}</p>
+                    </div>
+                @endif
+
+                <!-- Airport -->
+                @if (!empty($booking->airport_name))
+                    <div class="col-md-6">
+                        <strong>Airport:</strong>
+                        <p>{{ $booking->airport_name }}</p>
+                    </div>
+                @endif
+
+                <!-- Booking Date -->
+                @if (!empty($booking->created_at))
+                    <div class="col-md-6">
+                        <strong>Booking Date:</strong>
+                        <p>{{ \Carbon\Carbon::parse($booking->created_at)->format('d M, Y') }}</p>
+                    </div>
+                @endif
+
+                <!-- Vehicle Type -->
+                @if (!empty($booking->vehicle_type))
+                    <div class="col-md-6">
+                        <strong>Vehicle Type:</strong>
+                        <p>{{ $booking->vehicle_type }}</p>
+                    </div>
+                @endif
+
+                <!-- Status -->
+                @if (!empty($booking->status))
+                    <div class="col-md-6">
+                        <strong>Status:</strong>
+                        <p>{{ $booking->status }}</p>
+                    </div>
+                @endif
+
+                <!-- Pickup Address -->
+                @if (!empty($booking->pickup_address))
+                    <div class="col-md-6">
+                        <strong>Pickup Address:</strong>
+                        <p>{{ $booking->pickup_address }}</p>
+                    </div>
+                @endif
+
+                <!-- Dropoff Address -->
+                @if (!empty($booking->dropoff_address))
+                    <div class="col-md-6">
+                        <strong>Dropoff Address:</strong>
+                        <p>{{ $booking->dropoff_address }}</p>
+                    </div>
+                @endif
+
+                <!-- Pickup Date -->
+                @if (!empty($booking->pickup_date))
+                    <div class="col-md-6">
+                        <strong>Pickup Date:</strong>
+                        <p>{{ \Carbon\Carbon::parse($booking->pickup_date)->format('d M, Y') }}</p>
+                    </div>
+                @endif
+
+                <!-- Pickup Time -->
+                @if (!empty($booking->pickup_time))
+                    <div class="col-md-6">
+                        <strong>Pickup Time:</strong>
+                        <p>{{ $booking->pickup_time }}</p>
+                    </div>
+                @endif
+
+                <!-- Number of Adults -->
+                @if (!empty($booking->number_adults))
+                    <div class="col-md-6">
+                        <strong>Number of Adults:</strong>
+                        <p>{{ $booking->number_adults }}</p>
+                    </div>
+                @endif
+
+                <!-- Number of Children -->
+                @if (!empty($booking->number_children))
+                    <div class="col-md-6">
+                        <strong>Number of Children:</strong>
+                        <p>{{ $booking->number_children }}</p>
+                    </div>
+                @endif
+
+                <!-- Return Pickup Date -->
+                @if (!empty($booking->return_pickup_date))
+                    <div class="col-md-6">
+                        <strong>Return Pickup Date:</strong>
+                        <p>{{ $booking->return_pickup_date }}</p>
+                    </div>
+                @endif
+
+                <!-- Return Pickup Time -->
+                @if (!empty($booking->return_pickup_time))
+                    <div class="col-md-6">
+                        <strong>Return Pickup Time:</strong>
+                        <p>{{ $booking->return_pickup_time }}</p>
+                    </div>
+                @endif
+
+                <!-- More fields can be added in the same manner -->
             </div>
 
             <div class="mt-4">
