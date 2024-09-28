571c24232790b345ccf77bfa46b5a8f2a9be137f
unknown <s.adeboye@outlook.com>
2024-09-28 15:22:37 +0100

    added invoice-failed view

---
+++ resources/views/passenger/invoice-failed.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.passenger')
+
+@section('title', 'Payment Failed')
+
+@section('content')
+    <div class="alert alert-danger">
+        <h1>Payment Failed</h1>
+        <p>Unfortunately, we couldn't verify your payment. Please try again.</p>
+        <a href="{{ route('passenger.makepayments') }}" class="btn btn-primary">Go back to Payments</a>
+    </div>
+@endsection
