b7448c8b8d6e9d5e5fd4cc50c073105f9c3f8740
unknown <s.adeboye@outlook.com>
2024-09-30 06:11:21 +0100

    added contact model which also act as ticket model

---
+++ app/Models/Contact.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contact';
+
+    protected $fillable = [
+        'ticket_num', 'status', 'department', 'priority', 'attachment',
+        'fullname', 'email_address', 'subject', 'message'
+    ];
+    
+
+    // Relationship with TicketReply
+    public function replies()
+    {
+        return $this->hasMany(TicketReply::class, 'ticket_id');
+    }
+
+    // Relationship with User based on email address
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'email_address', 'email');
+    }
+}
