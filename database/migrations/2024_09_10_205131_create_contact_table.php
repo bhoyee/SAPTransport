27ed7e92eca9cf3f41c86dee3f0b27ad6e3d8da6
unknown <s.adeboye@outlook.com>
2024-09-30 06:14:56 +0100

    updated migration

--- database/migrations/2024_09_10_205131_create_contact_table.php
+++ database/migrations/2024_09_10_205131_create_contact_table.php
@@ -15,10 +15,14 @@ class CreateContactTable extends Migration
     {
         Schema::create('contact', function (Blueprint $table) {
             $table->id();  // Primary key
-            $table->enum('category', ['enquiry', 'feedback', 'emergency', 'query']);  // Contact message category
+            $table->string('ticket_num', 6)->unique();  // 6-digit ticket number
+            $table->enum('status', ['open', 'closed'])->default('open');  // Status of the ticket
+            $table->enum('department', ['support', 'sales', 'billing']);  // Department for the ticket
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');  // Priority of the ticket
+            $table->string('attachment')->nullable();  // File attachment for support ticket
             $table->string('fullname', 255);  // Full name of the person contacting
-            $table->string('phone_number', 15);  // Phone number
             $table->string('email_address', 100);  // Email address
+            $table->string('phone_number', 15)->nullable();  // Phone number (optional)
             $table->text('message');  // Message content
             $table->timestamps();  // created_at and updated_at
         });
