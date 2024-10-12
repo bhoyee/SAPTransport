102efc6e3071b69cc40ee2b87c4cf5a876a32dab
unknown <s.adeboye@outlook.com>
2024-10-12 06:49:55 +0100

    create email template for contact view

---
+++ app/Mail/ContactConfirmation.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactConfirmation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    public function build()
+    {
+        return $this->view('emails.contact_confirmation')
+                    ->with(['contact' => $this->contact]);
+    }
+}
---
+++ resources/views/emails/contact_confirmation.blade.php
@@ -0,0 +1,5 @@
+<h1>Thank you for contacting us!</h1>
+<p>Dear {{ $contact->fullname }},</p>
+<p>We have received your message and will get back to you shortly.</p>
+<p><strong>Message:</strong> {{ $contact->message }}</p>
+<p><strong>Ticket Number:</strong> {{ $contact->ticket_num }}</p>
