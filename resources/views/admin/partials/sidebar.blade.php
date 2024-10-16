1dd954f49dce6255b81e2d1ec626dc1adb5ac743
unknown <s.adeboye@outlook.com>
2024-10-16 11:27:18 +0100

    updated show view and siderbar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -173,9 +173,11 @@
                                 </li>
 
 								<li class="submenu-item">
-    <a class="submenu-link" href="{{ route('admin.bookings.confirm-search') }}">Confirm Booking</a>
-</li>
-
+									<a class="submenu-link" href="{{ route('admin.bookings.confirm-search') }}">Confirm Booking</a>
+								</li>
+								<li class="submenu-item">
+									<a class="submenu-link" href="">Booking Report</a>
+								</li>
 
 
                                 </ul>
@@ -185,36 +187,7 @@
                             </li>
 
 
-					    
-
-					        <li class="nav-item">
-
-                                    <a class="nav-link" href="{{ url('/') }}">
-
-                                        <span class="nav-icon">
-
-								<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-
-									<path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-
-									<path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-
-								  </svg>
-
-                                        </span>
-
-                                        <span class="nav-link-text">Booking</span>
-
-                                    </a>
-
-                                </li>
-
-
-
-
-
-
-
+			
 						<li class="nav-item has-submenu">
 
 					        <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
@@ -255,13 +228,43 @@
 							
 							        <li class="submenu-item"><a class="submenu-link" href="{{ route('passenger.makepayments') }}">Make Payment</a></li>
 
-							        <li class="submenu-item"><a class="submenu-link" href="{{ route('payment.history') }}">Payment History</a></li>
+							        <li class="submenu-item"><a class="submenu-link" href="{{ route('payment.history') }}">Manage Payments</a></li>
+
+									<li class="submenu-item">
+									<a class="submenu-link" href="">Payments Report</a>
+								</li>
 
 						        </ul>
 
 					        </div>
 
 					    </li><!--//nav-item-->
+
+
+								    
+
+						<li class="nav-item">
+
+							<a class="nav-link" href="{{ url('/') }}">
+
+								<span class="nav-icon">
+
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+
+							<path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+
+							<path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+
+							</svg>
+
+								</span>
+
+								<span class="nav-link-text">Booking</span>
+
+							</a>
+
+							</li>
+
 
 
 
