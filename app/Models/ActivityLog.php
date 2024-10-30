0aea1d24a46e3fb77baaa3aa6d991a6a8de09839
unknown <s.adeboye@outlook.com>
2024-10-30 06:17:17 +0000

    updated activity log model

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -1,13 +1,27 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class ActivityLog extends Model
-{
-    use HasFactory;
-
-    protected $fillable = ['user_id', 'action', 'description', 'ip_address'];
-}
+<?php
+
+
+// app/Models/ActivityLog.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class ActivityLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'action', 'description', 'ip_address'];
+
+    /**
+     * Relationship to get the user associated with the activity log.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
+
+
