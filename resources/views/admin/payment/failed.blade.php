78c6012571a10f3aab7613df900e8b043b472681
unknown <s.adeboye@outlook.com>
2024-11-07 14:42:26 +0000

    updated payments view and routes

--- resources/views/admin/payment/failed.blade.php
+++ resources/views/admin/payment/failed.blade.php
@@ -1,4 +1,10 @@
-@extends('admin.layouts.admin-layout')
+@php
+    $layout = auth()->user()->hasRole('admin') 
+        ? 'admin.layouts.admin-layout' 
+        : 'staff.layouts.staff-layout';
+@endphp
+
+@extends($layout)
 @section('content')
     <h1>Payment Failed</h1>
     <p>Unfortunately, your payment could not be processed. Please try again later.</p>
